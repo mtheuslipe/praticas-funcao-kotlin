cc7904d27eb1f582a0c336f1c9c1535325a945d9
mtheuslipe <matheusfelipe.mf454@gmail.com>
2022-12-15 15:01:31 -0300

    teste de algumas funções e fixação de argumentos vararg

--- src/main/kotlin/funcoes.kt
+++ src/main/kotlin/funcoes.kt
@@ -1,18 +1,17 @@
-fun sum(x: Int, y: Int): Int = (x+y)
-fun printMessageWithPrefix(vararg Message: String, prefix: String ="sei lá" ){
-    for(m in Message) println(prefix + Message)
+fun printNumbersAndSum(x: Int, y: Int): Int{
+    println(x)
+    println(y)
+    var sum: Int= x+y
+    println(sum)
+    return(sum)
 }
-
-
-
-
-
-
-
+fun printMessageWithPrefix(vararg Message: String, prefix: String ="sei lá" ){
+    for(m in Message) println(prefix + m)
+}
 fun main(){
-    sum(4,5)
-    printMessageWithPrefix("oi", "calma cara", "ela é linda")
-    printMessageWithPrefix("ela é linda", "amo ela" )
+    printNumbersAndSum(4,5)
+    printMessageWithPrefix(" oi", " calma cara", " ela é linda")
+    printMessageWithPrefix(" ela é linda", " amo ela" )
 
 
 }
